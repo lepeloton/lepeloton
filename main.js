f5ee9e94e9f74b06df61a18efbf8ee99bf4c2caf
spookgit <luke@minutebuzz.com>
2018-03-31 17:29:43 +0200

    fixed overscroll behaviour

--- main.js
+++ main.js
@@ -33,7 +33,6 @@
 
 
     var frames = document.getElementsByClassName('frames');
-console.log('FRAMES', frames)
 
     // SCROLLING VARIABLES
     var windowLoaded = false;
@@ -63,6 +62,8 @@ console.log('FRAMES', frames)
             canScroll = true;
         }, 2000)
 
+        heroVideo.playbackRate = 0.4;
+
     }
 
     var update = function () {
@@ -86,6 +87,26 @@ console.log('FRAMES', frames)
 
         frames[prevFrame - 1].classList.remove('active', 'up', 'down')
         frames[prevFrame - 1].classList.add(prevFramePosition)
+
+        if(frame === 3) {
+            heroVideo.style.opacity = 0;
+        } else if (heroVideo.style.opacity !== 1) {
+            heroVideo.style.opacity = 1;
+        }
+
+        if(frame === 5) {
+            Array.from(galleryImage).forEach(function(image, i) {
+                setTimeout(function() {
+                    image.classList.add('shown')
+                }, 300 * i)
+            })
+        }
+
+        if(frame !== 5 && galleryImage[0].classList.contains('shown') > 0) {
+            Array.from(galleryImage).forEach(function(image, i) {
+                image.classList.remove('shown')
+            })
+        }
     }
 
     // Scroll Section
@@ -94,6 +115,8 @@ console.log('FRAMES', frames)
 
 
     function scrollHandler(e) {
+        e.preventDefault();
+
         if(windowLoaded && canScroll === true) {
 
             if (e.touches) {
@@ -108,12 +131,10 @@ console.log('FRAMES', frames)
                 if (oldFinger && newFinger - oldFinger <= -50 && frame < frames.length) {
                     prevFrame = frame
                     frame ++
-console.log('FRAME', frame)
                     update()
                 } else if (newFinger - oldFinger >= 50 && frame > 1) {
                     prevFrame = frame
                     frame --
-console.log('FRAME', frame)
                     update()
                 }
                 lastFinger = newFinger
@@ -122,110 +143,13 @@ console.log('FRAME', frame)
                 prevFrame = frame
                 if (e.deltaY > 0 && frame < frames.length) {
                     frame ++
-console.log('FRAMEY', frame)
                     update()
                 } else if (e.deltaY < 0 && frame > 1) {
                     frame --
-console.log('FRAMEY', frame)
                     update()
                 }
                               
             }
         }
-
-    // Scroll Section
-    // window.onscroll = function() {
-    //     var scroll = scrollY;
-    //     console.log(scroll)
-
-    //     // transition the logo
-    //     logoFrame.style.top = 50 + scroll / windowHeight / 2 * 100 + "%";
-    //     logoFrame.style.opacity = 1 - scroll / windowHeight
-
-    //     // shift the fuzzy text
-    //     // if (scroll > fuzzy.offsetTop - windowHeight && scroll < fuzzy.offsetTop + windowHeight) {
-    //     //     fuzzyImage.style.transform = "translate(-40%, -50%)";
-    //     //     fuzzyText.style.top = scroll/2 + "px";
-    //     //     let trans = - 100 + scroll / windowHeight * 100 / 2
-    //     //     fuzzyText.style.transform = "translateY(" + trans + "%)";
-    //     // } else {
-    //     //     fuzzyImage.style.transform = "translate(-100%, -50%)";
-    //     // }
-
-    //     // Food video to top
-    //     if (scroll > food.offsetTop - windowHeight * 0.75) {
-    //         heroVideo.style.opacity = 0
-    //     } else {
-    //         heroVideo.style.opacity = 1
-    //     }
-
-    //     // map to top
-    //     if (scroll > map.offsetTop - windowHeight) {
-    //         map.style.zIndex = 7
-    //     } else {
-    //         map.style.zIndex = 0
-    //     }
-
-    //     // transition map link
-    //     if (scroll > map.offsetTop - windowHeight / 2) {
-    //         mapLink.style.opacity = 1
-    //     } else {
-    //         mapLink.style.opacity = 0
-    //     }
-
-    //     // transition images
-    //     if (scroll > gallery.offsetTop - windowHeight && galleryImage[0].style.opacity != 1) {
-    //         for (var i = 0; i < galleryImage.length; i++) {
-    //             timeout(i)
-    //         }
-    //     }
-
-    //     // transition images
-    //     // if (scroll > food.offsetTop - windowHeight) {
-    //     //     let trans = (scroll - food.offsetTop)
-    //     //     riders.style.bottom = trans + 'px'
-    //     // }
-
-    //     // fade in popbox
-    //     // if (scroll > fuzzy.offsetHeight - windowHeight / 2) {
-    //     //     popBlock.style.opacity = 1;
-    //     //     // popBlock.style.bottom = (scroll - popBlock.offsetHeight) / 3.5 + 'px'
-    //     // } 
-    //     // else {
-    //     //     popBlock.style.opacity = 0;
-    //     // }
-
-    //     // // control popins
-    //     // if (scroll > fuzzy.offsetTop - windowHeight / 2 && scroll < fuzzy.offsetTop + windowHeight / 2) {
-    //     //         popBlock1.style.height = 100 + "%";
-    //     //         popBlock1.style.width = 100 + "%";
-    //     //     popBlock2.style.height = 0 + "px";
-    //     //     popBlock2.style.width = 0 + "px";
-    //     //     popBlock3.style.height = 0 + "px";
-    //     //     popBlock3.style.width = 0 + "px";
-    //     // } else if (scroll > fuzzy.offsetTop + windowHeight / 2 && scroll < fuzzy.offsetTop + 3 * windowHeight / 2) {
-    //     //         popBlock2.style.height = 100 + "%";
-    //     //         popBlock2.style.width = 100 + "%";
-    //     //     popBlock1.style.height = 0 + "px";
-    //     //     popBlock1.style.width = 0 + "px";
-    //     //     popBlock3.style.height = 0 + "px";
-    //     //     popBlock3.style.width = 0 + "px";
-    //     // } else if (scroll > fuzzy.offsetTop + 3 * windowHeight / 2 && scroll < fuzzy.offsetTop + 5 * windowHeight / 2) {
-    //     //         popBlock3.style.height = 100 + "%";
-    //     //         popBlock3.style.width = 100 + "%";
-    //     //     popBlock1.style.height = 0 + "px";
-    //     //     popBlock1.style.width = 0 + "px";
-    //     //     popBlock2.style.height = 0 + "px";
-    //     //     popBlock2.style.width = 0 + "px";
-    //     // }
-
-    //     // pop fuzzy boxes
-
-    //     console.log('scroll', scroll - (fuzzy.offsetTop - windowHeight / 2), "pop", popBlocks[0].offsetHeight)
-    //     if (scroll > fuzzy.offsetTop - windowHeight / 2 && popBlocks[0].offsetHeight === 0) {
-    //         for (var i = 0; i < popBlocks.length; i++) {
-    //             timeout2(i)
-    //         }
-    //     }
     }
 }());
